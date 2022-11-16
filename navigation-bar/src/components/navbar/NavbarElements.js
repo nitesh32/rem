d8232c3321fb41ba21a0f2744518d200ac9bdb52
nitesh32 <nit8339@gmail.com>
2022-11-17 01:20:03 +0530

    doing

--- navigation-bar/src/components/navbar/NavbarElements.js
+++ navigation-bar/src/components/navbar/NavbarElements.js
@@ -1,4 +1,4 @@
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaBluetooth } from 'react-icons/fa';
 import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -8,8 +8,8 @@ position:fixed;
 width:100%;
 height: 85px;
 display: flex;
-justify-content: space-between;
-padding: 0.2rem calc((100vw - 1000px) / 2);
+justify-content: center;
+// padding: 0.2rem calc((100vw - 1000px) / 2);
 z-index: 12;
 /* Third Nav */
 /* justify-content: flex-start; */
@@ -30,7 +30,7 @@ cursor: pointer;
 
 export const Bars = styled(FaBars)`
 display: none;
-color: #808080;
+color: blue;
 @media screen and (max-width: 768px) {
 	display: block;
 	position: absolute;
@@ -45,16 +45,15 @@ color: #808080;
 export const NavMenu = styled.div`
 display: flex;
 align-items: center;
-margin-right: -24px;
 /* Second Nav */
 /* margin-right: 24px; */
 /* Third Nav */
 /* width: 100vw;
 white-space: nowrap; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
-`;
+// @media screen and (max-width: 768px) {
+// 	display: none;
+// }
+// `;
 
 export const NavBtn = styled.nav`
 display: flex;
@@ -86,3 +85,4 @@ margin-left: 24px;
 	color: #808080;
 }
 `;
+
--- navigation-bar/src/components/navbar/index.js
+++ navigation-bar/src/components/navbar/index.js
@@ -1,47 +1,34 @@
-import React from 'react';
+import React from "react";
 import {
-Nav,
-NavLink,
-Bars,
-NavMenu,
-NavBtn,
-NavBtnLink,
-} from './NavbarElements';
-
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  // NavBtn,
+  // NavBtnLink,
+} from "./NavbarElements";
 const Navbar = () => {
-return (
-	<>
-	<Nav position="static">
-		<Bars />
+  return (
+    <>
+      <Nav >
+        <Bars />
 
-		<NavMenu>
-		<NavLink to='/about' activeStyle>
-			About
-		</NavLink>
-		<NavLink to='/events' activeStyle>
-			Events
-		</NavLink>
-		<NavLink to='/annual' activeStyle>
-			Annual Report
-		</NavLink>
-		<NavLink to='/team' activeStyle>
-			Teams
-		</NavLink>
-		<NavLink to='/blogs' activeStyle>
-			Blogs
-		</NavLink>
-		<NavLink to='/sign-up' activeStyle>
-			Sign Up
-		</NavLink>
-		{/* Second Nav */}
-		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-		</NavMenu>
-		<NavBtn>
-		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
-		</NavBtn>
-	</Nav>
-	</>
-);
+        <NavMenu class="hov">
+          <NavLink to="/about" activeStyle>
+            Govt. Jobs
+          </NavLink>
+          <NavLink to="/events" activeStyle>
+            Competitions
+          </NavLink>
+          <NavLink to="/annual" activeStyle>
+			  Adminstration forms
+		  </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
