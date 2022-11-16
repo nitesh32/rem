2aefcaa238cafcafa7913e78b81b4c6045dad604
nitesh32 <nit8339@gmail.com>
2022-11-17 00:50:50 +0530

    done_nav

---
+++ navigation-bar/src/pages/about.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./all.css";
+
+const About = () => {
+return (
+	// <div
+	// style={{
+	// 	display: 'flex',
+	// 	justifyContent: 'Right',
+	// 	alignItems: 'Right',
+	// 	height: '100vh'
+	// }}
+	// >
+	// <h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+
+	// </div>
+<div class="cont">
+
+</div>
+);
+};
+
+export default About;
